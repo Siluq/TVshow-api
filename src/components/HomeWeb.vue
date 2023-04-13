21c2ec01c57491f68dca575ed0d1e10b3897c848
Sam Boesjes <sam.boesjes@gmail.com>
2023-04-13 16:03:28 +0200

    picture edded

--- src/components/HomeWeb.vue
+++ src/components/HomeWeb.vue
@@ -3,6 +3,13 @@
     <h1>Welkome</h1>
     <p>Bij deze de technische opdracht voor de rol Front-end developer.</p>
   </div>
+  <div class="foto">
+    <h2>Met vriendelijke groet, <br />Sam Boesjes</h2>
+    <img
+      src="https://www.dropbox.com/s/q89oj5egzvn3efa/DR_03.jpg?raw=1"
+      alt="Sam picture"
+    />
+  </div>
 </template>
 
 <script>
@@ -11,3 +18,13 @@ export default {
   props: {},
 };
 </script>
+
+<style>
+.foto {
+  object-fit: contain;
+  margin: auto;
+}
+img {
+  width: 25%;
+}
+</style>
